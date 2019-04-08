8e220a7737fd9b0f3e86eb60389645a69e490d59
Alymbekov <maxim.makarov.1997@mail.ru>
2019-04-09 00:05:46 +0600

    fixed problem : changed Meal and MealCategory model on 'on to one field'  on foreignkey

--- crm_food/admin.py
+++ crm_food/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from crm_food.models import (
+        Table, Role, MealCategory, Meal, Department,
+    )
+
+admin.site.register(Table)
+admin.site.register(Role)
+admin.site.register(MealCategory)
+admin.site.register(Meal)
+admin.site.register(Department)
+
+
+
 
 # Register your models here.
--- crm_food/models.py
+++ crm_food/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Table(models.Model):
+    name_of_tables = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name_of_tables
+
+
+class Role(models.Model):
+    name_of_roles = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name_of_roles
+
+
+class Department(models.Model):
+    name_of_departments = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name_of_departments
+
+
+class MealCategory(models.Model):
+    title = models.CharField(max_length=100)
+    department = models.ForeignKey('Department', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Meal(models.Model):
+    name_of_meal = models.CharField(max_length=100)
+    category = models.ForeignKey('MealCategory', on_delete=models.SET_NULL, null=True)
+    price = models.PositiveSmallIntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name_of_meal
+
+
