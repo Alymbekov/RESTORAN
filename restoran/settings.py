be3a28fb7631d7878ef9b6c366f987a7dcb99f18
Alymbekov <maxim.makarov.1997@mail.ru>
2019-04-27 17:06:54 +0600

    cover project to docker

--- restoran/settings.py
+++ restoran/settings.py
@@ -81,7 +81,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
-        'PASSWORD':config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'POST': config('DB_PORT'),
     }
