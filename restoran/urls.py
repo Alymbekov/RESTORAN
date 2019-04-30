1903468ff8c648cd1d69eefafd2ccbeaab82cdd5
Alymbekov <maxim.makarov.1997@mail.ru>
2019-04-30 16:27:21 +0600

    changes few names url and modified serializers

--- restoran/urls.py
+++ restoran/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('crm_food.urls')),
+    path('api/v1/', include('crm_food.urls')),
 ]
